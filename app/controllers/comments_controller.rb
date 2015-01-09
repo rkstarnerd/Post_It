3a00aae26e3f06ae0043785c94f4c029e84d9ddb
rkstarnerd <gemille.ford@gmail.com>
2015-01-08 22:48:45 -0500

    adds TODO for comment creator and removes user_id from params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,8 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(params.require(:comment).permit(:body, :user_id))
-    @comment.creator = User.first
+    @comment = @post.comments.build(params.require(:comment).permit(:body))
+    @comment.creator = User.first # TODO remove this line once user-login is set up
 
     if @comment.save
       flash[:notice] = "Your comment was added."
