6f2275a37e92087c3ad46cf9b77b186099f16f97
rkstarnerd <geille.ford@gmail.com>
2015-01-13 23:27:49 -0500

    add register, login, logout routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,22 +8,41 @@ class UsersController < ApplicationController
   end
 
   def new
-	
+    @user = User.new	
   end
 
   def edit
-
   end
 
   def create
+    @user = User.new(user_params)
 
+    if @user.save
+      flash[:notice] = "You have successfully registered."
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
+    @user = User.update(user_params)
 
+    if @user.save
+      flash[:notice] = "You have successfully updated your profie."
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :time_zone)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
 
+  get '/register', to: 'users#new'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
   resources :users, except: [:destroy] do
     resources :posts
     resources :comments, only: [:create, :show]
