243de16f2c3ec025f7cf80d93b6d0413f1cc0f82
rkstarnerd <geille.ford@gmail.com>
2015-02-04 23:08:28 -0500

    edit time_of_post method to define time zone based on user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,11 +3,13 @@ module ApplicationHelper
     url.starts_with?('http://' || 'https://') ? url : 'http://' + url
   end
 
-  def time_of_post(date)
-    if date.nil?
-      date
+  def time_of_post(dt)
+    # if dt.nil?
+    #   dt
+    if logged_in? && !current_user.time_zone.blank?
+      dt = dt.in_time_zone(current_user.time_zone).strftime("%A, %m/%d/%Y at %l:%M %P %Z")
     else
-      date.strftime("%A, %m/%d/%Y at %l:%M %P %Z")
+      dt = dt.strftime("%A, %m/%d/%Y at %l:%M %P %Z")
     end
   end
 end
