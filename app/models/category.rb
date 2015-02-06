21c4cbc2236f9ed29cc2c5dd8a9791d3672e75b3
rkstarnerd <geille.ford@gmail.com>
2015-02-05 20:32:08 -0500

    correct generate_slug method for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -7,7 +7,7 @@ class Category < ActiveRecord::Base
   validates :name, presence: true
 
   def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+    self.slug = self.name.gsub(' ', '-').downcase
   end
 
   def to_param
