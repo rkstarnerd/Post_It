7bedae130106b4689011a5ba597a5ff228427e2f
rkstarnerd <geille.ford@gmail.com>
2015-02-05 20:23:52 -0500

    create slug methods for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,15 @@ class Category < ActiveRecord::Base
   has_many :post_categories
   has_many :posts, through: :post_categories
 
+  before_save :generate_slug
+
   validates :name, presence: true
+
+  def generate_slug
+    self.slug = self.title.gsub(' ', '-').downcase
+  end
+
+  def to_param
+    self.slug
+  end
 end
