27142351696d6488c1889f95c13d1f35e14607a9
rkstarnerd <geille.ford@gmail.com>
2015-02-05 19:42:51 -0500

    add slug column to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205032611) do
+ActiveRecord::Schema.define(version: 20150206004142) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20150205032611) do
     t.string "password_digest"
     t.string "role"
     t.string "time_zone"
+    t.string "slug"
   end
 
   create_table "votes", force: true do |t|
