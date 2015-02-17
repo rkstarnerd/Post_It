6c8519116d307dde6e020f9a9d2a29301f0293c1
rkstarnerd <gemille.ford@gmail.com>
2015-02-16 21:34:56 -0500

    def generate_slug so that non-alphanumeric characters are removed

--- app/models/post.rb
+++ app/models/post.rb
@@ -23,7 +23,14 @@ class Post < ActiveRecord::Base
   end
 
   def generate_slug
-    self.slug = self.title.gsub(' ', '-').downcase
+    str = self.title
+    str = str.strip
+
+    #replace non alphanumeric characters with a dash
+    str.gsub! /\s*[^A-Za-z0-9]\s*/, '-'
+    #remove multiple, consecutive instances of a dash
+    str.gsub! /-+/, "-"
+    self.slug = str.downcase
   end
 
   def to_param
