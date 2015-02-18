0ca56351dd839f8993b5dab2e62847d42c78ba2a
Gemille Ford <gemille.ford@gmail.com>
2015-02-18 10:58:22 -0500

    require and include voteable_gemille

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
 
-  include Voteable
+  include VoteableGemille
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   belongs_to :post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 
-  include Voteable
+  include VoteableGemille
 
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
   has_many :comments, dependent: :destroy
