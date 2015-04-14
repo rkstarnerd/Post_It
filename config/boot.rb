ee3866c8558e8c6daf76a1d4f46c4349f238f41c
rkstarnerd <gemille.ford@gmail.com>
2015-04-14 19:16:06 -0400

    test relationships and validations

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,6 @@
+Encoding.default_external = 'UTF-8'
+Encoding.default_internal = 'UTF-8'
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+describe Post do
+  it { should belong_to(:creator) }
+  it { should have_many(:comments).dependent(:destroy) }
+  it { should have_many(:post_categories) }
+  it { should have_many(:categories).through(:post_categories) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
+
+end
