f6c338cc142313acc52da9af87d0ff90ef5890c5
rkstarnerd <gemille.ford@gmail.com>
2015-05-09 21:45:29 -0400

    add class name and foreign key to association test

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
 describe Vote do
-  it { should belong_to(:creator) }
+  it { should belong_to(:creator).class_name('User').with_foreign_key('user_id') }
   it { should belong_to(:voteable) }
 end
