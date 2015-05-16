a00afdaad5d2b05877eede9857f671e6515ac79d
rkstarnerd <gemille.ford@gmail.com>
2015-05-16 13:46:18 -0400

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221062405) do
+ActiveRecord::Schema.define(version: 20150516164758) do
 
   create_table "categories", force: true do |t|
     t.string "name"
@@ -49,6 +49,8 @@ ActiveRecord::Schema.define(version: 20150221062405) do
     t.string "slug"
     t.string "phone"
     t.string "pin"
+    t.string "provider"
+    t.string "uid"
   end
 
   create_table "votes", force: true do |t|
