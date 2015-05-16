e2fa4727a3c8203dfb54504b52c560356e43979c
rkstarnerd <gemille.ford@gmail.com>
2015-05-16 13:24:53 -0400

    def create_omniauth method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -27,6 +27,14 @@ class SessionsController <ApplicationController
     end
   end
 
+  def create_omniauth
+    auth = request.env["omniauth.auth"]
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    session[:user_id] = user.id
+    flash[:notice] = "You've logged in!"
+    redirect_to root_path
+  end
+
   def pin
     access_denied if session[:two_factor].nil?
 
